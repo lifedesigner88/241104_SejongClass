9db973ae7058232b1f3491b7cce919276a70e379
SejongPark <lifedesigner88@gmail.com>
2024-11-11 17:54:01 +0900

    프론트 초기화

--- FrontEnd_Client/src/views/HomeView.vue
+++ FrontEnd_Client/src/views/HomeView.vue
@@ -1,9 +1,40 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from "vue";
+
+const createUserData = ref({
+      name: '',
+      email: '',
+      password: ''
+    }
+);
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+const createResult = ref('');
+const createUser = async () => {
+
+  const response = await fetch(`${BASE_URL}/user`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(createUserData.value)
+  });
+  createResult.value = await response.json();
+
+}
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
+    <form>
+      <input v-model="createUserData.name" placeholder="userName"/>
+      <input v-model="createUserData.email" placeholder="userEmail"/>
+      <input v-model="createUserData.password" placeholder="userPassword"/>
+      <button @click.prevent="createUser">Sign Up</button>
+    </form>
+
+
   </main>
 </template>
